af1e238e8aeebb6ee5ebb93db6d930a5e4db0f29
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-02-19 07:42:21 -0300

    Pegando dados (index.js) e gerando um INSERT.sql (builder.js)

---
+++ src/builder.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const format = require('pg-format');
+const games = require('./games.json')
+const league = 'italia'
+
+function matches(games) {
+  const scores = games.scores.map( (score) => ( score.split(":") ) )
+  const corners = games.corners.map( (ele) => ele.split(":") )
+  const date = games.date.map(element => '20'+element.replace(element.substr(8), '') )
+  const base = []
+  let indexScore = 0
+  games.home.forEach( (element, i) => {    
+    base.push(
+      [
+        date[i].trim(), [element.trim(), games.away[i].trim() ], "PL",
+        [scores[indexScore][0].trim(), scores[indexScore][1].trim()],
+        [scores[indexScore+1][0].trim(), scores[indexScore+1][1].trim()],
+        [corners[indexScore+1][0].trim(), corners[indexScore+1][1].trim()],
+        [corners[indexScore][0].trim(), corners[indexScore][1].trim()]
+      ]
+    )
+      indexScore+=2
+  });
+  return base
+}
+const data = matches(games)
+
+sql = format(`INSERT INTO ${league}(game_date, team_home, team_away,
+                home_ft, away_ft, home_ht, away_ht, league,
+                corners_home_ht, corners_away_ht, corners_home_ft, corners_away_ft) 
+              VALUES %L`, data); 
+
+
+fs.writeFileSync(__dirname+'/insert.sql', sql, (err) => {
+  if (err) throw err;
+})
---
+++ src/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+const fs = require('fs')
+
+async function start (){
+  
+  async function getText(page, selector){
+    const text = await page.$$eval(selector, texts => texts.map( text => text.innerText));    
+    return text
+  }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.URL);
+  
+  const home = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-right.BR0 a');
+  const away = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-left a');
+  const scores = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.red-color')
+  const corners = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.blue-color')
+  const date = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td:nth-child(2)')
+
+  fs.writeFile('games.json', JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
+    if(err) throw new Error('ERROR')
+    console.log('Done');
+
+  })
+
+  await browser.close();
+}
+
+start()
