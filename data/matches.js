d94b4de852474a4ef6a24cc717b7e1fa5adcdfca
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-02-20 18:34:01 -0300

    Pegando dados do site, gerando um JSON e colocando no diretório data/

---
+++ data/matches.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+const nameJson = readlineSync.question('VIEW $NAME_JSON.json): ');
+const games = require(`../data/${nameJson}.json`)
+const scores = games.scores.map( (score) => ( score.split(":") ) )
+const corners = games.corners.map( (ele) => ele.split(":") )
+const date = games.date.map(element => '20'+element.replace(element.substr(8), '') )
+
+let indexScore = 0
+games.home.forEach( (element, i) => {    
+    console.log( `${date[i].trim()} | ${element.trim()} ${scores[indexScore][0].trim()} x  ${scores[indexScore][1].trim()} ${games.away[i].trim()} | HT ${scores[indexScore+1][0].trim()} X  ${scores[indexScore+1][1].trim()} CORNERS FT: ${corners[indexScore+1][0].trim()} X ${corners[indexScore+1][1].trim()}  HT: ${corners[indexScore][0].trim()}X${corners[indexScore][1].trim()}`      
+    )
+      indexScore+=2
+  });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 const fs = require('fs')
+
+const nameJson = readlineSync.question('Generated JSON (data/): ') || 'data';
 
 async function start (){
   
+  async function part(page, numberPart){
+    const part = parseInt(numberPart, 10) || 1
+    await page.select('#panel-1 > #ended #pager_select', `/league/35/p.${part}?type=ended_race`)  
+    await page.waitForSelector('#panel-1 > #ended #pager_select')
+    await page.click('#panel-1 > #ended #pager_select')  
+  }
+
   async function getText(page, selector){
     const text = await page.$$eval(selector, texts => texts.map( text => text.innerText));    
     return text
@@ -11,7 +21,12 @@ async function start (){
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(process.env.URL);
+  await page.goto(process.env.URL);  
+  await page.setViewport({ width: 1366, height: 673 })  
+
+  const resultsPage = readlineSync.question('Results Page (default = 1):  ') || 1;
+  await part(page, resultsPage)
+  await page.waitForTimeout(4000)
   
   const home = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-right.BR0 a');
   const away = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-left a');
@@ -19,13 +34,12 @@ async function start (){
   const corners = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.blue-color')
   const date = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td:nth-child(2)')
 
-  fs.writeFile('data.json', JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
-    if(err) throw new Error('ERROR')
-    console.log('Done');
+  fs.writeFile(`${__dirname}/../data/${nameJson}.json`, JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
+    if(err) throw new Error('Cannot find mudole')
+    console.log(`Generated: ../data/${nameJson}.json `);
 
   })
 
   await browser.close();
 }
-
 start()
