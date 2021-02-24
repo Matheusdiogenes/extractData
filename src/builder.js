6b36bb505ecc509c99bd4884dd67254de0d5175d
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-02-24 08:18:00 -0300

    todos os arquivos gerados irão para o diretório data/

--- src/builder.js
+++ src/builder.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const readlineSync = require('readline-sync');
 const format = require('pg-format');
 
-const nameJson = readlineSync.question('Get JSON data/: ') || 'data';
-const games = require(`../data/${nameJson}.json`)
+const nameJson = readlineSync.question('Get JSON data/json/: ') || 'data';
+const games = require(`../data/json/${nameJson}.json`)
 
 function matches(games) {
   const scores = games.scores.map( (score) => ( score.split(":") ) )
@@ -14,7 +14,7 @@ function matches(games) {
   games.home.forEach( (element, i) => {    
     base.push(
       [
-        date[i].trim(), [element.trim(), games.away[i].trim() ], "PL",
+        date[i].trim(), [element.trim(), games.away[i].trim() ],
         [scores[indexScore][0].trim(), scores[indexScore][1].trim()],
         [scores[indexScore+1][0].trim(), scores[indexScore+1][1].trim()],
         [corners[indexScore+1][0].trim(), corners[indexScore+1][1].trim()],
@@ -28,11 +28,12 @@ function matches(games) {
 const data = matches(games)
 
 const tableName = readlineSync.question('Name of Table: ') || 'table';
+
 sql = format(`INSERT INTO ${tableName}(game_date, team_home, team_away,
-                home_ft, away_ft, home_ht, away_ht, league,
-                corners_home_ht, corners_away_ht, corners_home_ft, corners_away_ft) 
+                home_ft, away_ft, home_ht, away_ht,
+                corners_home_ft, corners_away_ft, corners_home_ht, corners_away_ht) 
               VALUES %L`, data); 
 
-fs.writeFileSync(__dirname+`/${nameJson}.sql`, sql, (err) => {
-  if (err) throw err;
+fs.writeFileSync(__dirname+`/../data/sql/${nameJson}.sql`, sql, (err) => {
+  if (err) throw err;  
 })
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
 const fs = require('fs')
 
-const nameJson = readlineSync.question('Generated JSON (data/): ') || 'data';
+const nameJson = readlineSync.question('Generated JSON (data/json/): ') || 'data';
 const resultsPage = readlineSync.question('Results Page (default = 1):  ') || 1;
 
 async function start (){
   
   async function part(page, numberPart){
     const part = numberPart || '1'
-    await page.setViewport({ width: 1366, height: 673 })  
+    await page.setViewport({ width: 1366, height: 1000 })  
+    
     await page.select('#panel-1 > #ended #pager_select', `/league/${process.env.LEAGUE_ID}/p.${part}?type=ended_race`)  
     await page.waitForSelector('#panel-1 > #ended #pager_select')
+    await page.waitForTimeout(3000)
     await page.click('#panel-1 > #ended #pager_select')  
   }
 
@@ -26,7 +28,7 @@ async function start (){
   await page.goto(process.env.URL);  
 
   await part(page, resultsPage)
-  await page.waitForTimeout(4000)
+  
   
   const home = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-right.BR0 a');
   const away = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-left a');
@@ -34,9 +36,9 @@ async function start (){
   const corners = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.blue-color')
   const date = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td:nth-child(2)')
 
-  fs.writeFile(`${__dirname}/../data/${nameJson}.json`, JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
+  fs.writeFile(`${__dirname}/../data/json/${nameJson}.json`, JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
     if(err) throw new Error('Cannot find mudole')
-    console.log(`Generated: ../data/${nameJson}.json `);
+    console.log(`Generated JSON: ../data/json/${nameJson}.json `);
 
   })
 
