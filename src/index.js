1d04a8fbf14cc9948602a2ad84f38c6f880bc7d9
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-04-03 19:39:17 -0300

    um time só

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ const readlineSync = require('readline-sync');
 const fs = require('fs')
 
 const nameJson = readlineSync.question('Generated JSON (data/json/): ') || 'data';
-const resultsPage = readlineSync.question('Results Page (default = 1):  ') || 1;
 
 async function start (){
-  
-  async function part(page, numberPart){
-    const part = numberPart || '1'
-    await page.setViewport({ width: 1366, height: 1000 })  
     
-    await page.select('#panel-1 > #ended #pager_select', `/league/${process.env.LEAGUE_ID}/p.${part}?type=ended_race`)  
-    await page.waitForSelector('#panel-1 > #ended #pager_select')
-    await page.waitForTimeout(3000)
-    await page.click('#panel-1 > #ended #pager_select')  
+  async function part(page, numberPart=1){    
+    await page.setViewport({ width: 1366, height: 10000 })      
+    // await page.select('#panel-1 > #ended #pager_select', `/league/${process.env.LEAGUE_ID}/p.${part}?type=ended_race`)  
+    // await page.click('#panel-1 > #ended #pager_select')  
   }
 
   async function getText(page, selector){
@@ -25,16 +20,16 @@ async function start (){
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(process.env.URL);  
+  await page.goto('https://www.scorebing.com/team/6267');  
 
-  await part(page, resultsPage)
+  await part(page)
+  await page.screenshot({path: 'buddy-screenshot.png'})
   
-  
-  const home = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-right.BR0 a');
-  const away = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-left a');
-  const scores = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.red-color')
-  const corners = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td.text-center.blue-color')
-  const date = await getText(page, 'section#ended table.live-list-table.diary-table tbody tr td:nth-child(2)')
+  const home = await getText(page, '#ended .live-list-table.diary-table tbody tr td.text-right.BR0 a');  
+  const away = await getText(page, '#ended .live-list-table.diary-table tbody tr td.text-left a');
+  const scores = await getText(page, '#ended .live-list-table.diary-table tbody tr td.text-center.red-color')
+  const corners = await getText(page, '#ended .live-list-table.diary-table tbody tr td.text-center.blue-color')
+  const date = await getText(page, '#ended .live-list-table.diary-table tbody tr td:nth-child(2)')
 
   fs.writeFile(`${__dirname}/../data/json/${nameJson}.json`, JSON.stringify({home: home, away: away, scores: scores, corners: corners, date: date}, null, 2), err => {
     if(err) throw new Error('Cannot find mudole')
